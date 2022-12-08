7f911ff6304ca87d305b7fd6565b58697cfeb720
Sultvn <shavkatovs@yandex.com>
2022-12-08 12:27:22 +0500

    some changes on table

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Footer from "./layouts/footer/Footer";
 import Faq from "./pages/faq/Faq";
 import Apply from "./pages/page4/Page4";
 import { Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/about/Main.layout";
-import Tabs from "./components/tab/Tabs";
-import data1 from './components/tab/tabsType1.json'
+import CareerPage from "./pages/careerPage/CareerPage";
 
 function App() {
   return (
-    <div className="App">
-      <MainLayout>
-      <Tabs {...data1}/>
+    <div className="">
+      <header>
+        <Navbar />
+      </header>
 
-      </MainLayout>
+      <CareerPage />
+
+      <main>
+        <Routes>
+          <Route path="/" />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/apply" element={<Apply />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
   );
 }
